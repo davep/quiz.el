94555a69fa8a2f9c43fa717d1cadab27bc7e43cd
Dave Pearson <davep@davep.org>
2017-08-16 15:24:58 +0100

    Add support for selecting categories

--- quiz.el
+++ quiz.el
@@ -50,8 +50,11 @@
   "Face for the question."
   :group 'quiz)
 
-(defconst quiz-source-url "https://opentdb.com/api.php?amount=%d&encode=base64%s"
+(defconst quiz-source-url "https://opentdb.com/api.php?amount=%d&encode=base64%s%s"
   "URL for loading up questions from the Open Trivia DB.")
+
+(defconst quiz-categories-url "https://opentdb.com/api_category.php"
+  "URL for loading up the list of quiz categories.")
 
 (defconst quiz-difficulty-levels '("any" "easy" "medium" "hard")
   "Levels of difficulty of questions.")
@@ -62,9 +65,50 @@
 (defconst quiz-buffer-name "*Quiz*"
   "Name of the quiz buffer.")
 
+(defun quiz-get (url)
+  "Get quiz data from URL."
+  (let* ((url-request-extra-headers `(("User-Agent" . ,quiz-user-agent)))
+         (buffer (url-retrieve-synchronously url t)))
+    (when buffer
+      (with-current-buffer buffer
+        (set-buffer-multibyte t)
+        (setf (point) (point-min))
+        (when (search-forward-regexp "^$" nil t)
+          (buffer-substring (1+ (point)) (point-max)))))))
+
+(defvar quiz-categories nil
+  "Holds the list of quiz categories once they've been loaded.
+
+Never access this directly, always call `quiz-get-categories' instead.")
+
+(defun quiz-lispify-categories (json-categories)
+  "Turn JSON-CATEGORIES into a list."
+  (let ((categories (make-hash-table :test #'equal)))
+    (cl-loop for cat across (alist-get 'trivia_categories (json-read-from-string json-categories))
+             do (puthash (alist-get 'name cat) (alist-get 'id cat) categories)
+             finally return categories)))
+
+(defun quiz-get-categories ()
+  "Return the list of quiz categories."
+  (or quiz-categories
+      (setq quiz-categories (quiz-lispify-categories (quiz-get quiz-categories-url)))))
+
+(defun quiz-get-category-names ()
+  "Return a list of category names."
+  (sort
+   (cl-loop for cat being the hash-key of (quiz-get-categories) collect cat)
+   #'string<))
+
 (defun quiz-lispify-questions (json-questions)
   "Turn JSON-QUESTIONS into a list."
   (alist-get 'results (json-read-from-string json-questions)))
+
+(defun quiz-category-url-param (category)
+  "Return CATEGORY as a parameter for the quiz URL."
+  (let ((id (gethash category (quiz-get-categories))))
+    (if (null id)
+        ""
+      (format "&category=%d" id))))
 
 (defun quiz-difficulty-url-param (difficulty)
   "Return DIFFICULTY as a parameter for the quiz URL."
@@ -72,23 +116,19 @@
       ""
     (format "&difficulty=%s" difficulty)))
 
-(defun quiz-get-questions (&optional count difficulty)
+(defun quiz-get-questions (&optional count category difficulty)
   "Load COUNT questions from the trivia server.
 
 Ten questions are loaded if COUNT isn't supplied.
 
+Specify CATEGORY to only get questions in that category.
+
 DIFFICULTY can be used top optionally set the difficulty of the questions."
-  (let* ((url-request-extra-headers `(("User-Agent" . ,quiz-user-agent)))
-         (buffer (url-retrieve-synchronously
-                  (format quiz-source-url
-                          (or count 10)
-                          (quiz-difficulty-url-param difficulty)))))
-    (when buffer
-      (with-current-buffer buffer
-        (set-buffer-multibyte t)
-        (setf (point) (point-min))
-        (when (search-forward-regexp "^$" nil t)
-          (quiz-lispify-questions (buffer-substring (1+ (point)) (point-max))))))))
+  (quiz-lispify-questions
+   (quiz-get (format quiz-source-url
+                     (or count 10)
+                     (quiz-category-url-param category)
+                     (quiz-difficulty-url-param difficulty)))))
 
 (defun quiz-decode (s)
   "Decode S."
@@ -129,11 +169,13 @@ DIFFICULTY can be used top optionally set the difficulty of the questions."
   (quiz-insert-answers questions i)
   (insert "\n"))
 
-(defun quiz-insert-questions (count difficulty)
+(defun quiz-insert-questions (count category difficulty)
   "Get and insert COUNT questions into the current buffer.
 
+Optionally only show questions in the given CATEGORY.
+
 Questions will be at most as hard as DIFFICULTY."
-  (let ((questions (quiz-get-questions count difficulty)))
+  (let ((questions (quiz-get-questions count category difficulty)))
     (if questions
         (cl-loop for i from 0 to (1- (length questions))
                  do (quiz-insert-question questions i))
@@ -196,13 +238,14 @@ The key bindings for `quiz-mode' are:
   "Holds the questions for the current quiz.")
 
 ;;;###autoload
-(defun quiz (count difficulty)
+(defun quiz (count category difficulty)
   "Play a multiple choice trivia quiz with COUNT questions.
 
 Questions will be at most as hard as DIFFICULTY."
   (interactive
    (list
     (read-number "Questions: " 10)
+    (completing-read "Category: (default any)" (quiz-get-category-names) nil t)
     (completing-read
      (format "Difficulty (default %s): " (car quiz-difficulty-levels))
      quiz-difficulty-levels nil t nil nil (car quiz-difficulty-levels))))
@@ -216,7 +259,7 @@ Questions will be at most as hard as DIFFICULTY."
         (let ((buffer-read-only nil))
           (setf (buffer-string) "")
           (save-excursion
-            (setq quiz-questions (quiz-insert-questions count difficulty))
+            (setq quiz-questions (quiz-insert-questions count category difficulty))
             (quiz-insert-finish))
           (widget-forward 1))
         (switch-to-buffer buffer)))))
